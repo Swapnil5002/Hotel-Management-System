0914ffeef8b053b5628b000c6cad77053524b27d
Swapnil5002 <healthytopups.com>
2023-04-16 23:52:35 +0530

    error boundary path correction

--- hms/src/App.js
+++ hms/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import withErrorBoundary from './withErrorBoundary';
+import withErrorBoundary from './HOC/error-boundary';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
